ed564683235a2accfd7e102a75eb05c3790bda2e
samer <khsamer11@gmail.com>
2023-02-10 23:09:29 +0200

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,19 @@
+/*
+GLOBALS  - NO WINDOW !!!
+
+__direname   - path to current directory
+--filename   - file name
+require      - dfunction to use moduls (commonJS)
+module       - info about current module (file)
+process      - info about env where the program is being executed
+
+*/
+
+console.log(__dirname);
+console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process);
+// setInterval(() => {
+//     console.log('hello world');
+// }, 1000)
---
+++ 3-modules.js
@@ -0,0 +1,15 @@
+// Modules
+// commonJS, every file in node is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const {john, peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+// console.log(data);
+// sayHi('susan');
+// sayHi(john);
+// sayHi(peter);
+
